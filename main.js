2e92273f67e8347a9a1fabfa028db7ad43ff0c7a
navfal2010 <navfal808@gmail.com>
2024-05-02 11:14:28 +0500

    done

---
+++ main.js
@@ -0,0 +1,6 @@
+let Wrapper = document.querySelector('.wrapper')
+let first = prompt('Ismingizni kiriting')
+let p = document.createElement('p')
+p.innerHTML = first
+p.style.color = 'yellow'
+Wrapper.appendChild(p)
